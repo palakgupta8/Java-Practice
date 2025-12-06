9efb96ec671f916297cb150d174de43257637d25
palakgupta8 <itspalak02@gmail.com>
2025-12-06 23:24:23 +0530

    Arr

--- Method.java
+++ Method.java
@@ -1,5 +1,5 @@
  class Calculator{
-     int add(int n1, int n2){
+     int add(int n1, int n2){//method
         return n1+n2;
     }
 }
@@ -7,8 +7,8 @@
 
 public class Method{
     public static void main(String args[]){
-Calculator calc = new Calculator();
-int result=calc.add(4,5);
+Calculator calc = new Calculator();// creating object
+int result=calc.add(4,5);//calling method
 System.out.println("sum: " + result);
     }
 }
